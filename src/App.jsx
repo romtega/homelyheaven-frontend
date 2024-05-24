e269e0e2870ae64e6699ab7bc79ce8f76bda3d7a
1106 <sanders.gesture-0c@icloud.com>
2024-05-24 10:01:21 -0600

    Test enviroment deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 /* eslint-disable space-before-function-paren */
 /* eslint-disable semi */
 /* eslint-disable semi */
-import UserForm from "./components/UserForm/UserForm.jsx"
-
 function App() {
   return (
     <>
-      <UserForm/>
+      <h1>Hello word</h1>
     </>
   );
 }

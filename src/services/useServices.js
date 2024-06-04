2c4d6416f8562add80320dab273645929ee582ea
Roman Ortega Mondaca <romtega88@gmail.com>
2024-06-03 20:06:43 -0600

    Environment variables

--- src/services/useServices.js
+++ src/services/useServices.js
@@ -8,12 +8,4 @@ const loginUserService = (data) => {
   return axiosInstance.post("api/v1/login", data);
 };
 
-// const getMeUserService = (token) => {
-//   return axiosInstance.get("api/v1/users/:userId", {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//   });
-// };
-
 export { registerUserService, loginUserService };

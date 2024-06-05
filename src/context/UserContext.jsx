26264952d7d9ef26c69f882507e61cee1cf94540
1106 <sanders.gesture-0c@icloud.com>
2024-06-05 16:27:36 -0600

    userContext test02

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,20 +1,19 @@
-/* eslint-disable comma-dangle */
 import { createContext, useState, useEffect } from "react";
-import { getUserService } from "@/services/useServices";
+import { getUserService } from "@/services/userService";
 
 const UserContext = createContext();
 
-function UserProvider ({ children }) {
+function UserProvider({ children }) {
   const [userData, setUserData] = useState({});
   const [loading, setLoading] = useState(true);
 
   const fetchUserData = async () => {
     try {
       const response = await getUserService();
+      console.log("Response:", response); // para el test
       setUserData(response.data);
-      console.log(userData);
     } catch (error) {
-      console.error("Error al cargar datos del usuario", error);
+      console.error("Error al cargar datos del usuario:", error.response || error);
     } finally {
       setLoading(false);
     }
--- src/services/useServices.js
+++ src/services/useServices.js
@@ -4,13 +4,20 @@ const registerUserService = (data) => {
   return axiosInstance.post("api/v1/register", data);
 };
 
-const loginUserService = (data) => {
-  return axiosInstance.post("api/v1/login", data);
+const loginUserService = async (data) => {
+  const response = await axiosInstance.post("api/v1/login", data);
+  const { token } = response.data;
+  if (token) {
+    localStorage.setItem("token", token);
+  }
+  return response;
 };
 
 const getUserService = () => {
   const token = localStorage.getItem("token");
+  console.log("Retrieved Token:", token); // token x2 para test
   if (!token) {
+    console.error("Token not found");
     return Promise.reject("Token not found");
   }
 

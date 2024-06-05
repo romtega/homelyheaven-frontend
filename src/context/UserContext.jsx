7322af27aa19c2caa5c700c6ccb4467c3c226c40
1106 <sanders.gesture-0c@icloud.com>
2024-06-05 16:35:03 -0600

    little typo

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { getUserService } from "@/services/userService";
+import { getUserService } from "@/services/useServices";
 
 const UserContext = createContext();
 

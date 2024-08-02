324ee87fe46c41a681e5160cdf5f31f2a6f0a45a
Roman Ortega Mondaca <romtega88@gmail.com>
2024-08-02 21:46:44 +0200

    feat: test route to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import { BrowserRouter } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axiosInstance from "./services/axiosConfig";
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import RoutesIndex from "@/routes/RoutesIndex";
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axiosInstance
+      .get("/test-endpoint")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }, []);
+
   return (
     <BrowserRouter>
       <Navbar />
       <RoutesIndex />
       <Footer />
+      <div className="app__test">
+        <h2>Prueba de Comunicación con el Backend</h2>
+        {error && <p>Error: {error}</p>}
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      </div>
     </BrowserRouter>
   );
 };
--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,9 +1,10 @@
 /* eslint-disable comma-dangle */
-// import axios from "axios";
+import axios from "axios";
 
-// const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL,
-//   timeout: 10000,
-// });
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 10000,
+  headers: { "Content-Type": "application/json" },
+});
 
-// export default axiosInstance;
+export default axiosInstance;

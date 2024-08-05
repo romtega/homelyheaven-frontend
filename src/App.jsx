755cf824e22bccb6a8b5fa2a3cdcf31270025dce
Roman Ortega Mondaca <romtega88@gmail.com>
2024-08-05 17:34:59 +0200

    feat: improve login form validation and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,30 @@ import Footer from "@/components/Footer";
 import RoutesIndex from "@/routes/RoutesIndex";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
+  // const [data, setData] = useState(null);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    axiosInstance
-      .get("/test-endpoint")
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axiosInstance
+  //     .get("/test-endpoint")
+  //     .then((response) => {
+  //       setData(response.data);
+  //     })
+  //     .catch((error) => {
+  //       setError(error.message);
+  //     });
+  // }, []);
 
   return (
     <BrowserRouter>
       <Navbar />
       <RoutesIndex />
       <Footer />
-      <div className="app__test">
+      {/* <div className="app__test">
         <h2>Prueba de Comunicación con el Backend</h2>
         {error && <p>Error: {error}</p>}
         {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-      </div>
+      </div> */}
     </BrowserRouter>
   );
 };
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,19 +1,58 @@
 import "./loginform.css";
+
+import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axiosInstance from "@/services/axiosConfig";
+import { jwtDecode } from "jwt-decode";
 
 const LoginForm = () => {
+  const [isAuth, SetIsAuth] = useState(false);
+  const [userPayload, setUserPayload] = useState(null);
+
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
+  const onSubmit = async (data) => {
+    try {
+      const response = await axiosInstance.post("/api/v1/login", data);
+
+      if (!response || !response.data || response.status !== 200) {
+        throw new Error(
+          `Error ${response.status}: ${
+            response.data?.msg || "Unexpected error occurred"
+          }`
+        );
+      }
+
+      const { token } = response.data;
+      localStorage.setItem("token", token);
+
+      const payload = jwtDecode(token);
+      SetIsAuth(true);
+      setUserPayload(payload);
+      navigate("/user/profile");
+      alert("¡Bienvenido!");
+    } catch (error) {
+      console.error("Login error:", error.message);
+      alert(
+        error.response?.data?.msg ||
+          "Ocurrió un error durante el inicio de sesión. Por favor, intenta nuevamente."
+      );
+    }
+  };
+
   return (
     <section className="section__signin grid">
       <form
         className="signin__form flex font-base"
         noValidate // Desactivar validación del navegador
+        onSubmit={handleSubmit(onSubmit)}
       >
         <h2 className="signin__title font-2">Bienvenido</h2>
         <div className="signin__group grid">
